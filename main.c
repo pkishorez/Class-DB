7136fef0acfb82ef13f6aaa81038f6083d7f04a2
Kishore <kishore.iiitn@gmail.com>
2015-06-06 20:47:58 +0530

    Setup Class-DB project.
    `README` file consists of all the details of project layout.
    `libraries` folder consists of all supported standalone libraries for the libraries.
    `tests` folder consists of tests of all libraries.
    `Makefile` defines rules for compiling project.

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Kishore
+ *
+ * Created on June 5, 2015, 2:53 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+	return (EXIT_SUCCESS);
+}
+
