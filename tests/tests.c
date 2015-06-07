055cf2d17f42e218bdfae2764acab6afa3d44367
Kishore <kishore.iiitn@gmail.com>
2015-06-07 21:41:11 +0530

    Add testing framework to the project along with rules to compile test files.
    tests.c and tests.h is center for all other tests files that runs all the tests.
    Unit testing to libraries allow `perfection over abstraction` philosophy of the project. Seatest testing framework for c is incorporated into Class-DB because it is simple and independent library and do not club with the project. Testing is completely independent of the development code.

---
+++ tests/tests.c
@@ -0,0 +1,16 @@
+/**
+ * @file tests.c
+ * @author Kishore
+ * @date June 7, 2015, 3:16 PM
+ * 
+ * @brief All tests of all libraries
+ */
+
+#include "seatest/seatest.h"
+#include "tests.h"
+#include "../libraries/headers/headers.h"
+
+void main()
+{
+	test_bnf();
+}
---
+++ tests/tests.h
@@ -0,0 +1,7 @@
+/**
+ * @file tests.h
+ * @author Kishore
+ * @date June 7, 2015, 3:17 PM 
+ */
+
+void test_bnf();
