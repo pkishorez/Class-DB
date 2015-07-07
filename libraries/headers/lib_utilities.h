489881ba61e3b83e5e0c57c61625f79d86f0eda7
Kishore <kishore.iiitn@gmail.com>
2015-07-07 22:40:41 +0530

    Add more functions to utilities library.

--- libraries/headers/lib_utilities.h
+++ libraries/headers/lib_utilities.h
@@ -14,3 +14,14 @@
 	}
 
 int U_streq(char *s1, char *s2);
+
+uint16_t lrotate16(uint16_t num, int k);
+uint32_t lrotate32(uint32_t num, int k);
+uint64_t lrotate64(uint64_t num, int k);
+
+int is_little_endian();
+int is_big_endian();
+
+uint16_t to_bigendian16(uint16_t num);
+uint32_t to_bigendian32(uint32_t num);
+uint64_t to_bigendian64(uint64_t num);
--- libraries/lib_utilities.c
+++ libraries/lib_utilities.c
@@ -10,8 +10,7 @@
  * more frequently used functions are stored in utilities library.
  */
 
-#include <stdio.h>
-#include <string.h>
+#include "headers/headers.h"
 #include "headers/lib_utilities.h"
 
 /**
@@ -35,4 +34,169 @@ int U_streq(char *s1, char *s2)
 		s2++;
 	}
 	return 1;
+}
+
+
+
+/**
+ * Circular left rotate of 16 bit integer.
+ * @param num : number that should be rotated.
+ * @param k : number of bits that should be rotated.
+ * @returns the number rotated k bits.
+ * 
+ * The number given in parameter and returned are both
+ * of type uint16_t. It left rotates the bits and returns
+ * the result.
+ */
+uint16_t lrotate16(uint16_t num, int k)
+{
+	return (num<<k) | (num>>(16-k));
+}
+
+
+
+/**
+ * Circular left rotate of 32 bit integer.
+ * @param num : number that should be rotated.
+ * @param k : number of bits that should be rotated.
+ * @returns the number rotated k bits.
+ * 
+ * The number given in parameter and returned are both
+ * of type uint32_t. It left rotates the bits and returns
+ * the result.
+ */
+uint32_t lrotate32(uint32_t num, int k)
+{
+	//num = to_bigendian32(num);
+	return num = (num<<k) | (num>>(32-k));
+	//return to_bigendian32(num);
+}
+
+
+
+/**
+ * Circular left rotate of 64 bit integer.
+ * @param num : number that should be rotated.
+ * @param k : number of bits that should be rotated.
+ * @returns the number rotated k bits.
+ * 
+ * The number given in parameter and returned are both
+ * of type uint64_t. It left rotates the bits and returns
+ * the result.
+ */
+uint64_t lrotate64(uint64_t num, int k)
+{
+	return (num<<k) | (num>>(64-k));
+}
+
+
+
+/**
+ * Check if the system is little endian.
+ * @returns 1 if little endian and 0 if it is not.
+ */
+int is_little_endian()
+{
+	unsigned int i = 1;
+	return (int)*((char*)&i);
+}
+
+
+
+/**
+ * Check if the system is big endian.
+ * @returns 1 if big endian and 0 if it is not.
+ * 
+ * This function is just the negation of `is_little_endian()` function.
+ */
+int is_big_endian()
+{
+	return !is_little_endian();
+}
+
+
+
+/**
+ * Converts 64bit little endian number `num` to big endian.
+ * @param num : number that should be converted to big endian.
+ * @returns number that is converted to big endian.
+ * 
+ * Note that conversion happens only if the system is little endian.
+ * In case of Big endian system this function returns the same
+ * number without any modification.
+ */
+uint64_t to_bigendian64(uint64_t num)
+{
+	if (is_little_endian())
+	{
+		char *ch = (char *)&num;
+		int i=0;
+		for (i=0; i<=3; i++)
+		{
+			int tmp = ch[i];
+			ch[i] = ch[7-i];
+			ch[7-i] = tmp;
+		}
+		return num;
+	}
+	else
+		return num;
+}
+
+
+
+/**
+ * Converts 32bit little endian number `num` to big endian.
+ * @param num : number that should be converted to big endian.
+ * @returns number that is converted to big endian.
+ *
+ * Note that conversion happens only if the system is little endian.
+ * In case of Big endian system this function returns the same
+ * number without any modification.
+ */
+uint32_t to_bigendian32(uint32_t num)
+{
+	if (is_little_endian())
+	{
+		char *ch = (char *)&num;
+		int i=0;
+		for (i=0; i<=1; i++)
+		{
+			int tmp = ch[i];
+			ch[i] = ch[3-i];
+			ch[3-i] = tmp;
+		}
+		return num;
+	}
+	else
+		return num;
+}
+
+
+
+/**
+ * Converts 16bit little endian number `num` to big endian.
+ * @param num : number that should be converted to big endian.
+ * @returns number that is converted to big endian.
+ * 
+ * Note that conversion happens only if the system is little endian.
+ * In case of Big endian system this function returns the same
+ * number without any modification.
+ */
+uint16_t bigendian16(uint16_t num)
+{
+	if (is_little_endian())
+	{
+		char *ch = (char *)&num;
+		int i=0;
+		for (i=0; i<=0; i++)
+		{
+			int tmp = ch[i];
+			ch[i] = ch[1-i];
+			ch[1-i] = tmp;
+		}
+		return num;
+	}
+	else
+		return num;
 }
