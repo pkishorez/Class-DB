584e2cc3c5b42b5748c09e69851a51397769a049
Kishore <kishore.iiitn@gmail.com>
2016-01-17 19:37:40 +0530

    Moved all the definitions of libraries into one header file to avoid errors when included multiple times.

--- libraries/headers/lib_types.h
+++ libraries/headers/lib_types.h
@@ -4,7 +4,7 @@
  * @date October 25, 2015, 1:48 PM 
  */
 
-#ifndef MODULES_TYPES
+#ifndef LIB_MODULES_TYPES
 
 	/**
 	 * =============
@@ -52,6 +52,12 @@
 		int len;
 		int _index;
 	}bnf_cap;
+	
+	typedef struct
+	{
+		int bnf_parsed;
+		int status;
+	}bnf_rstatus_t;
 
 	typedef struct{
 		memory_get ref;
@@ -59,8 +65,8 @@
 	}bnf_mcap;
 
 	enum bnf_errors{
-		ERROR_PARSE					=	-1,
-		ERROR_STRING_INCOMPLETE		=	-2
+		BNF_ERROR_PARSE					=	-1,
+		BNF_ERROR_STRING_INCOMPLETE		=	-2
 	};
 
 	/**
@@ -126,10 +132,125 @@
 	#define assert_msg(expr, msg) \
 		if (!(expr))\
 		{\
-			printf("File : %s :%d: %s\n", __FILE__, __LINE__ , msg);\
+			ansi_error("File : %s :%d: %s\n", __FILE__, __LINE__ , msg);\
 			return 0;\
 		}
 
 
-	#define MODULES_TYPES 1
+	/**
+	 * ===========
+	 * BSON MODULE
+	 * ===========
+	 */
+
+	#define BSON_SINT32 "\x10"
+	#define BSON_SINT64 "\x12"
+	#define BSON_SSTRING "\x02"
+	#define BSON_SDOCUMENT "\x03"
+	#define BSON_SBINARY "\x05"
+	#define BSON_SUNKNOWN "\x00"
+
+	#define BSON_INT32 '\x10'
+	#define BSON_INT64 '\x12'
+	#define BSON_STRING '\x02'
+	#define BSON_DOCUMENT '\x03'
+	#define BSON_BINARY '\x05'
+	#define BSON_UNKNOWN '\x00'
+
+	typedef memory_get bson_document;
+	
+	typedef memory_get bson_array;
+	
+	typedef struct{
+		memory_get pointer;
+		int type;
+	}bson_property;
+	
+	/**
+	 * =================
+	 * DATA MODEL MODULE
+	 * =================
+	 */
+	enum {
+		DATATYPE_INT32 = 1,
+		DATATYPE_INT64,
+		DATATYPE_STRING,
+		DATATYPE_BINARY,
+		DATATYPE_BSON
+	};
+	
+	
+	/**
+	 * ===============
+	 * TERMINAL MODULE
+	 * ===============
+	 */
+	typedef struct{
+		int isspecial;
+		int ch;
+		int ctrl;
+		int shift;
+		int alt;
+	}keystroke_t;
+	
+	enum {
+		CHAR_TAB = 9,
+		CHAR_BACKSPACE = 127,
+		CHAR_ENTER = 10,
+		CHAR_ESCAPE = 27,
+		CHAR_UP = -200,
+		CHAR_DOWN,
+		CHAR_LEFT,
+		CHAR_RIGHT,
+		CHAR_HOME,
+		CHAR_END,
+		CHAR_F2,
+		CHAR_F3,
+		CHAR_DELETE
+	};
+	
+	/**
+	 * ====================
+	 * BNF_EXTENDED LIBRARY
+	 * ====================
+	 */
+	typedef struct bnfextended_struct{
+		char *common;
+		char *decision;
+		int isend;
+		struct bnfextended_struct **next;
+	}bnfextended_t;
+	
+	typedef struct bnfextended_pointer{
+		char* str;
+		struct bnfextended_pointer *next;
+	}bnfextended_pointer_t;
+
+	enum bnfextended_status{
+		BNFEXTENDED_COMPLETE,
+		BNFEXTENDED_SUGGESTION_NOT_FOUND,
+		BNFEXTENDED_SUGGESTION_FOUND,
+		BNFEXTENDED_EXCEEDED
+	};
+
+	/**
+	 * ===========================
+	 * DATA MODEL PROPERTY LIBRARY
+	 * ===========================
+	 */
+	
+	enum property_types{
+		DM_PROPERTY_INT32 = 1,
+		DM_PROPERTY_INT64 = 2,
+		DM_PROPERTY_NUMBER = 3,
+		DM_PROPERTY_STRING = 4,
+		DM_PROPERTY_BINARY = 5
+	};
+	typedef struct {
+		enum property_types type;
+		int offset;
+		int length;
+	}dm_property_t;
+
+	#define LIB_MODULES_TYPES 1
 #endif
